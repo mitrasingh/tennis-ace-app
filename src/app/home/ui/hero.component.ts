3b901659bd496d2481293983030085989799762c
mitrasingh <mitra.coder@gmail.com>
2025-03-02 22:18:38 -0500

    feat: add styling for component

--- src/app/home/ui/hero.component.ts
+++ src/app/home/ui/hero.component.ts
@@ -2,7 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  template: ``,
-  styles: [``],
+  template: `
+    <div class="container">
+      <img src="tennis-ace-banner.jpg" alt="Tennis Image" />
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        height: 100vh;
+        width: 50vw;
+      }
+      .container img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    `,
+  ],
 })
 export class HeroComponent {}
