6d24cf36362f3887af1720750ff51036ab2af412
mitrasingh <mitra.coder@gmail.com>
2025-03-03 20:36:37 -0500

    feat: component displays title of app and action buttons for login and signup

---
+++ src/app/home/ui/calltoaction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: `app-calltoaction`,
+  template: ` <div></div> `,
+  styles: [],
+  imports: [],
+})
+export class CallToActionComponent {}
