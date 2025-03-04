1d9c6e3aff7856f5545c1733ef15aca1fe60429b
mitrasingh <mitra.coder@gmail.com>
2025-03-03 22:31:57 -0500

    feat: import CallToAction component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { HeroComponent } from './ui/hero.component';
+import { CallToActionComponent } from './ui/calltoaction.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  imports: [MatButtonModule, HeroComponent],
+  styleUrls: ['./home.component.css'],
+  imports: [HeroComponent, CallToActionComponent],
 })
 export class HomeComponent {}
