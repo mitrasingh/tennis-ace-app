d228f0bb9d5e11066447bbac9bf24de155f89663
mitrasingh <mitra.coder@gmail.com>
2025-03-04 21:05:00 -0500

    feat: add logic for login component and related imports

--- src/app/home/ui/login-form/login-form.component.ts
+++ src/app/home/ui/login-form/login-form.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { Login } from '../../../models/login.model';
 @Component({
   selector: `app-login-form`,
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
-  imports: [],
+  imports: [ReactiveFormsModule],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  formData = output<Login>();
+  isSubmitted = false;
+  fb = new FormBuilder();
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  // Getter to access form control easier and more readable in the template
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  emitData(): void {
+    this.isSubmitted = true;
+    if (this.loginForm.valid) {
+      this.formData.emit(this.loginForm.getRawValue());
+    } else {
+      return console.log('error');
+    }
+  }
+}
