de47b9d6b0740fb5afc3a24906b0960a207fb3dc
mitrasingh <mitra.coder@gmail.com>
2025-03-04 21:16:12 -0500

    chore: update calltoaction component name to loginformcomponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './ui/hero.component';
-import { CallToActionComponent } from './ui/calltoaction.component';
+import { LoginFormComponent } from './ui/login-form.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [HeroComponent, CallToActionComponent],
+  imports: [HeroComponent, LoginFormComponent],
 })
 export class HomeComponent {}
