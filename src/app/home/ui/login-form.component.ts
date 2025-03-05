fa39d2be64f964f450c66d33822e995e3eb3d568
mitrasingh <mitra.coder@gmail.com>
2025-03-04 20:12:28 -0500

    refactor: rename calltoaction component to login-form component

---
+++ src/app/home/ui/login-form.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: `app-calltoaction`,
+  template: `
+    <div class="container">
+      <div class="right-container">
+        <h1>Tennis Ace</h1>
+        <p>Find a tennis buddy within New York City!</p>
+        <div class="button-container">
+          <button mat-raised-button>Login</button>
+          <button mat-raised-button>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        height: 100vh;
+        width: 40vw;
+      }
+      .right-container {
+        background-color: #34495e;
+        color: white;
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .button-container {
+        display: flex;
+        margin-top: 1.5em;
+        gap: 1em;
+      }
+
+      h1 {
+        font-size: 3em;
+      }
+    `,
+  ],
+  imports: [MatButtonModule],
+})
+export class CallToActionComponent {}
