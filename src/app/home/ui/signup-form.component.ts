7279e8c62a2d10587a60013361eea24759a7e8b1
mitrasingh <mitra.coder@gmail.com>
2025-03-04 20:17:32 -0500

    feat: new component for sign up form

---
+++ src/app/home/ui/signup-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: `app-signup-form`,
+  template: `<div>sign up form</div>`,
+  styles: [``],
+  imports: [],
+})
+export class SignupFormComponent {}
