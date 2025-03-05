b04194e3e58bf20ae12d14625e41e80c13a19967
mitrasingh <mitra.coder@gmail.com>
2025-03-04 21:37:49 -0500

    chore: update LoginFormComponent import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './ui/hero.component';
-import { LoginFormComponent } from './ui/login-form.component';
-
+import { LoginFormComponent } from './ui/login-form/login-form.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
