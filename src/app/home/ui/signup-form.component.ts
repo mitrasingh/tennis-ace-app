82339b7ae1dfc9d58b4a7c149c60b296d626e159
mitrasingh <mitra.coder@gmail.com>
2025-03-13 21:29:03 -0400

    chore: delete file no longer needed

--- src/app/home/ui/signup-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: `app-signup-form`,
-  template: `<div>sign up form</div>`,
-  styles: [``],
-  imports: [],
-})
-export class SignupFormComponent {}
