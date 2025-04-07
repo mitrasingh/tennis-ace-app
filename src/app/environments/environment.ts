a2c2de7354025dea07f9dd13c9f8d13d9bc6df25
mitrasingh <mitra.coder@gmail.com>
2025-04-07 15:37:45 -0400

    feat: create environment for supabase

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://rtfnmlfzetefuredfhae.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Zm5tbGZ6ZXRlZnVyZWRmaGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNDkzMzgsImV4cCI6MjA1OTYyNTMzOH0.s_NAzNhYO1nu2NqVG63jL8oX2CaClL3ud4zAXu-hXCE',
+};
